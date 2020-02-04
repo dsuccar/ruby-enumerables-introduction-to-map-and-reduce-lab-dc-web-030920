6ac84074142415a4c17e6a2f55f18a6c5ed51117
Daniel Succar <danielsuccar@icloud.com>
2020-02-04 20:47:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -68,5 +68,14 @@ end
 
 
 def reduce_to_any_true(source_array)
+  i = 0
+  answer = 0
+  while i < source_array.length
+    if source_array[i] == true
+      return true
+    end
 
+    i += 1
+  end
+return true
 end
