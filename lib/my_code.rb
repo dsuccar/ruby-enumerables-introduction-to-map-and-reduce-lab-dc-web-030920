68f640ee1f87b9e3511005e95c0264861251e93c
Daniel Succar <danielsuccar@icloud.com>
2020-02-04 20:46:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,12 +59,11 @@ def reduce_to_all_true(source_array)
   while i < source_array.length
     if source_array[i] == false
       return false
-    else
-      return true
-
     end
+    return true
     i += 1
   end
+
 end
 
 
