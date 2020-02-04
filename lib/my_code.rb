21fbc36b2931788d69a3e42ba833816d7d23939f
Daniel Succar <danielsuccar@icloud.com>
2020-02-04 20:37:03 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,13 +41,12 @@ def map_to_square(source_array)
 end
 
 
-def reduce_to_total(source_array, starting_point = 0)
+def reduce_to_total(array, starting_point = 0)
 
   i = 0
   answer = starting_point
   while i < source_array.length
     answer += source_array[i]
-
     i += 1
   end
   answer
@@ -56,7 +55,15 @@ end
 
 
 def reduce_to_all_true(source_array)
-
+  i = 0
+  answer = 0
+  while i < source_array.length
+    if source_array[i] == true
+      return 'be_truthy'
+    end
+    i += 1
+  end
+  answer
 end
 
 
